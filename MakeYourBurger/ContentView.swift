fd1cfe5a373ef5c76b8cc3992a04e3e9216bc2cb
Sree Sai Raghava <raghava.rym@gmail.com>
2020-04-21 18:06:08 +0530

    Initial Commit

---
+++ MakeYourBurger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MakeYourBurger
+//
+//  Created by Sree Sai Raghava Dandu on 21/04/20.
+//  Copyright © 2020 Sree Sai Raghava. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
